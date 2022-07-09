83d4436ede4b776899e206cdc647ce88b99e9007
Alfred Leandro Liang <s10223157@connect.np.edu.sg>
2022-07-10 00:58:01 +0800

    First pushes for the classes

---
+++ RazorPagesMovie/Admin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie
+{
+    public class Admin
+    {
+        public int adminID { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public int saltValue { get; set; }
+
+        public Admin() { }
+        public Admin(int id, string Password, string emailAddress, string Username, int SaltValue)
+        {
+            adminID = id;
+            password = Password;
+            email = emailAddress;
+            username = Username;
+            saltValue = SaltValue;
+        }
+        public string CreateHash(string password, int saltValue)
+        {
+            // COME BACK TO THIS LATER
+            return "";
+        }
+        public string DeleteReview(Review review)
+        {
+            // COME BACK TO THIS LATER
+            return "";
+        }
+        public void CreateItem(Item item)
+        {
+            // COME BACK TO THIS LATER
+            return;
+        }
+        public void DeleteItem(Item item)
+        {
+            // COME BACK TO THIS LATER
+            return;
+        }
+        public void EditItem(Item item)
+        {
+            // COME BACK TO THIS LATER
+            return;
+        }
+    }
+}
---
+++ RazorPagesMovie/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie
+{
+    public class Client
+    {
+        public int clientID { get; set; } // = (GLOBAL CLiENT ID VARIABLE) ADD THIS LATER !!!!!!!!!!!!
+        public string password { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public int saltValue { get; set; }
+        public List<Review> reviewList { get; set; }
+        public Client() { }
+        public Client(int ClientID, string Password, string Email, string Username, int SaltValue)
+        {
+            clientID = ClientID;
+            password = Password;
+            email = Email;
+            username = Username;
+            saltValue = SaltValue;
+        }
+        public string CreateHash(string Password, int randNum)
+        {
+            // !!! CHANGE THIS LATER
+            return "";
+        }
+        public void CreateReview(Review Review)
+        {
+            // !!! CHANGE THIS LATER
+        }
+        public int TotalReviews(List<Review> reviewList)
+        {
+            return reviewList.Count;
+        }
+    }
+}
