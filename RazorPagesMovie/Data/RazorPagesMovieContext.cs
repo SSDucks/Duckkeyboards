ef117d08c5dff8d92cf59a9e35d0146582b72413
ChuaHZ <s10222140@connect.np.edu.sg>
2022-08-02 21:56:45 +0800

    Removed Seeding

--- RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -13,9 +13,9 @@ namespace RazorPagesMovie.Data
 {
     public class RazorPagesMovieContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly RoleManager<ApplicationRole> _roleManager;
+        //private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+        //private readonly UserManager<ApplicationUser> _userManager;
+        //private readonly RoleManager<ApplicationRole> _roleManager;
         public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
             :base (options)
         {
@@ -38,44 +38,44 @@ namespace RazorPagesMovie.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
 
-            builder.Entity<ApplicationRole>().HasData(new List<ApplicationRole>
-            {
-                new ApplicationRole{
-                    Id = "1",
-                    Name = "Auditor",
-                    CreatedDate = DateTime.Now,
-                    Description = "(CRUD) access to audits",
-                    NormalizedName = "AUDITOR",
-                },
-                new ApplicationRole{
-                    Id = "2",
-                    Name = "Role Administrator",
-                    CreatedDate = DateTime.Now,
-                    Description = "(CRUD) assignment of roles to users and groups",
-                    NormalizedName = "ROLE ADMINISTRATOR",
-                },
-                new ApplicationRole{
-                    Id = "3",
-                    Name = "Shopkeeper",
-                    CreatedDate = DateTime.Now,
-                    Description = "(CRUD) Managers items/products",
-                    NormalizedName = "SHOPKEEPER",
-                },
-            });
-            var hasher = new PasswordHasher<ApplicationUser>();
-            builder.Entity<ApplicationUser>().HasData(new List<ApplicationUser>
-            {
-                new ApplicationUser{
-                    Id = "1",
-                    UserName = "admin@admin.com",
-                    Email = "admin@admin.com",
-                    NormalizedUserName = "ADMIN@ADMIN.COM",
-                    NormalizedEmail = "ADMIN@ADMIN.COM",
-                    EmailConfirmed = false,
-                    PasswordHash = hasher.HashPassword(null, "admin1")
-                },
+            //builder.Entity<ApplicationRole>().HasData(new List<ApplicationRole>
+            //{
+            //    new ApplicationRole{
+            //        Id = "1",
+            //        Name = "Auditor",
+            //        CreatedDate = DateTime.Now,
+            //        Description = "(CRUD) access to audits",
+            //        NormalizedName = "AUDITOR",
+            //    },
+            //    new ApplicationRole{
+            //        Id = "2",
+            //        Name = "Role Administrator",
+            //        CreatedDate = DateTime.Now,
+            //        Description = "(CRUD) assignment of roles to users and groups",
+            //        NormalizedName = "ROLE ADMINISTRATOR",
+            //    },
+            //    new ApplicationRole{
+            //        Id = "3",
+            //        Name = "Shopkeeper",
+            //        CreatedDate = DateTime.Now,
+            //        Description = "(CRUD) Managers items/products",
+            //        NormalizedName = "SHOPKEEPER",
+            //    },
+            //});
+            //var hasher = new PasswordHasher<ApplicationUser>();
+            //builder.Entity<ApplicationUser>().HasData(new List<ApplicationUser>
+            //{
+            //    new ApplicationUser{
+            //        Id = "1",
+            //        UserName = "admin@admin.com",
+            //        Email = "admin@admin.com",
+            //        NormalizedUserName = "ADMIN@ADMIN.COM",
+            //        NormalizedEmail = "ADMIN@ADMIN.COM",
+            //        EmailConfirmed = false,
+            //        PasswordHash = hasher.HashPassword(null, "admin1")
+            //    },
 
-            });
+            //});
         }
 
 
