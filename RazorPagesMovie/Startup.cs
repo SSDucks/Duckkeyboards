abe3a4ac149ed0f1a94707c1d65ef3ad83eb4f5f
ChuaHZ <s10222140@connect.np.edu.sg>
2022-08-03 21:08:00 +0800

    changed error page

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -108,7 +108,6 @@ namespace RazorPagesMovie
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
